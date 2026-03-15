a670a49414ff97f6bb2f3c5d11cd97f7d6a655fb
Carlos André <147477857+dzim-almeida@users.noreply.github.com>
2026-03-15 14:36:48 -0300

    Replace API key and image path with placeholders
    Updated API key and image path placeholders.

--- gemini-api/main.py
+++ gemini-api/main.py
@@ -2,19 +2,19 @@ from google import genai
 from PIL import Image
 import os
 
-client = genai.Client(api_key="AIzaSyCvW458zpcLtLKDRmVXRPls5676awk2A8k")
+client = genai.Client(api_key="your-api-key-here")
 
 def main():
 
     try:
-        image_path = os.path.join("images", "plane-001.jpeg")
+        image_path = os.path.join("your-image-here")
         image = Image.open(image_path)
     except Exception as e:
         print(f"Erro ao carregar a imagem: {e}")
         return
     
     prompt = [
-        "Classifique o objeto na imagem e forneça uma descrição detalhada.",
+        "your-question-here",
         image
     ]
 
